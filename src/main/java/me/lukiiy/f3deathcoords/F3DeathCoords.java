647401a8e82b5de82deb6678222933d103f49126
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-01-03 17:38:23 -0300

    Update 1.2!
    * Updated to work with F3Mod 1.2!

--- src/main/java/me/lukiiy/f3deathcoords/F3DeathCoords.java
+++ src/main/java/me/lukiiy/f3deathcoords/F3DeathCoords.java
@@ -1,26 +1,26 @@
 package me.lukiiy.f3deathcoords;
 
+import me.lukiiy.f3mod.DebugEntry;
 import me.lukiiy.f3mod.F3Mod;
+import me.lukiiy.f3mod.F3ModAPI;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.ClientPlayerEntity;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
-public class F3DeathCoords implements ClientModInitializer {
+public class F3DeathCoords implements ClientModInitializer, F3ModAPI {
     public static final Logger LOGGER = LoggerFactory.getLogger("f3deathcoords");
 
     public static Vec3d deathCoords = null;
 
     @Override
-    public void onInitializeClient() {
-        F3Mod.registerProvider(F3Mod.Section.LEFT, (p, w) -> {
-            if (deathCoords == null) return Collections.emptyList();
-            return Collections.singletonList("§cDeath XYZ: §f" + String.format("%s %s %s", (int) deathCoords.x, (int) deathCoords.y, (int) deathCoords.z));
-        });
-    }
+    public void onInitializeClient() {}
 
     public static void set(double x, double y, double z) {
         deathCoords = Vec3d.create(x, y, z);
@@ -28,5 +28,21 @@ public class F3DeathCoords implements ClientModInitializer {
 
     public static void reset() {
         deathCoords = null;
+    }
+
+    @Override
+    public Collection<DebugEntry> getEntries() {
+        return Collections.singleton(new DebugEntry() {
+            @Override
+            public F3Mod.Section getSection() {
+                return F3Mod.Section.LEFT;
+            }
+
+            @Override
+            public List<String> provide(ClientPlayerEntity clientPlayerEntity, World world) {
+                if (deathCoords == null) return Collections.emptyList();
+                return Collections.singletonList("§cDeath XYZ: §f" + String.format("%s %s %s", (int) deathCoords.x, (int) deathCoords.y, (int) deathCoords.z));
+            }
+        });
     }
 }
